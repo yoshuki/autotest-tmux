93074594b1406d8b44368de48c1985011861b577
MIKAMI Yoshiyuki <yoshuki@saikyoline.jp>
2010-10-15 02:04:29 +0900

    Supported "RailsRspec2".

--- lib/autotest/tmux.rb
+++ lib/autotest/tmux.rb
@@ -26,8 +26,13 @@ class Autotest::Tmux
     :red    => ['white', 'red']
   }
 
-  def self.message(msg=statusright, color=:black)
-    col = SCREEN_COLOR[color]
+  @last_result = {}
+
+  def self.message(msg=statusright, sc=:black)
+    msg ||= statusright       # when nil given
+    sc  ||= :black            # when nil given
+
+    col = SCREEN_COLOR[sc]
     msg = "#[fg=#{col[0]},bg=#{col[1]}] #{msg} #[default]" unless msg == statusright
     send_cmd(msg)
   end
@@ -37,8 +42,7 @@ class Autotest::Tmux
   end
 
   def self.run_tmux_session?
-    cmd = "#{tmux_cmd} has-session"
-    system cmd
+    system "#{tmux_cmd} has-session"
   end
 
   def self.execute?
@@ -52,12 +56,8 @@ class Autotest::Tmux
   def self.tmux_cmd=(tc); @tmux_cmd = tc; end
 
   def self.send_cmd(msg)
-    cmd = "#{tmux_cmd} set status-right '#{msg.gsub("'", "\'")}'"
-    system cmd
-    nil
+    system "#{tmux_cmd} set status-right '#{msg.gsub("'", "\'")}'"
   end
-
-  @last_message = {}
 
   # All blocks return false, to execute each of following blocks defined in user's own ".autotest".
 
@@ -104,23 +104,23 @@ class Autotest::Tmux
       num_failures, num_errors = results.map{|r| r.to_i}
 
       if num_failures > 0 || num_errors > 0
-        @last_message = {:message => "Red F:#{num_failures} E:#{num_errors}", :color => :red}
+        @last_result = {:message => "Red F:#{num_failures} E:#{num_errors}", :color => :red}
       else
-        @last_message = {:message => 'All Green', :color => :green}
+        @last_result = {:message => 'All Green', :color => :green}
       end
-    when 'Autotest::Rspec', 'Autotest::Rspec2', 'Autotest::RailsRspec', 'Autotest::MerbRspec'
+    when 'Autotest::Rspec', 'Autotest::Rspec2', 'Autotest::RailsRspec', 'Autotest::RailsRspec2', 'Autotest::MerbRspec'
       results = output.scan(/(\d+)\s*examples?,\s*(\d+)\s*failures?(?:,\s*(\d+)\s*pendings?)?/).first
       num_examples, num_failures, num_pendings = results.map{|r| r.to_i}
 
       if num_failures > 0
-        @last_message = {:message => "Fail F:#{num_failures} P:#{num_pendings}", :color => :red}
+        @last_result = {:message => "Fail F:#{num_failures} P:#{num_pendings}", :color => :red}
       elsif num_pendings > 0
-        @last_message = {:message => "Pend F:#{num_failures} P:#{num_pendings}", :color => :yellow}
+        @last_result = {:message => "Pend F:#{num_failures} P:#{num_pendings}", :color => :yellow}
       else
-        @last_message = {:message => 'All Green', :color => :green}
+        @last_result = {:message => 'All Green', :color => :green}
       end
     else
-      @last_message = {:message => "Unknown class name. (#{class_name})", :color => :red}
+      @last_result = {:message => "Unknown class. (#{class_name})"}
     end
     next false
   end
@@ -147,7 +147,7 @@ class Autotest::Tmux
 
   Autotest.add_hook :waiting do |at, *args|
     sleep 0.5
-    message @last_message[:message], @last_message[:color] if execute?
+    message @last_result[:message], @last_result[:color] if execute?
     next false
   end
 end
--- spec/autotest-tmux_spec.rb
+++ spec/autotest-tmux_spec.rb
@@ -1,10 +1,36 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Autotest::Tmux do
-  context 'all added hooks should be called by autotest' do
+  context 'All added hooks should be called by autotest' do
     Autotest::HOOKS.keys.each do |hook|
       subject { Autotest::ALL_HOOKS }
       it { should include(hook) }
     end
+  end
+
+  describe 'When display message' do
+    context 'without params' do
+      it { Autotest::Tmux.message.should be_true }
+    end
+
+    context 'with String as message' do
+      it { Autotest::Tmux.message('foo').should be_true }
+    end
+
+    context 'with nil as message' do
+      it { Autotest::Tmux.message(nil).should be_true }
+    end
+
+    context 'with Symbol as color' do
+      it { Autotest::Tmux.message('foo', :black).should be_true }
+    end
+
+    context 'with nil as color' do
+      it { Autotest::Tmux.message('foo', nil).should be_true }
+    end
+  end
+
+  after(:all) do
+    Autotest::Tmux.clear
   end
 end
